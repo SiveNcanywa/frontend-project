16f69dac1b56d1e81ac9aeeff79e360225cb0eff
SiveNcanywa <jeandreross@gmail.com>
2022-03-09 14:59:21 +0200

    first push

---
+++ src/components/home.vue
@@ -0,0 +1,57 @@
+<template>
+  <nav class="navbar navbar-light bg-light">
+  <div class="container-fluid">
+    <span class="navbar-brand mb-0 h1">GABS</span>
+    <ul class="nav nav-pills justify-content-end">
+  <li class="nav-item">
+    <a class="nav-link active" aria-current="page" href="#">Home</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">About</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">Contact</a>
+  </li>
+  
+</ul>
+  </div>
+</nav>
+<h1 class="heading">Golden Arrow Bus Services</h1>
+<h4 class="slogan">The Bus for us</h4>
+<div class="logo">
+    <img src="https://i.postimg.cc/BnHD34YM/front-removebg-preview.png" alt="logo">
+</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.heading{
+    font-family: muli,sans-serif;
+    font-size: 70px;
+    font-weight: 700;
+    position: relative;
+    left: -25%;
+    margin-top: 15%;
+}
+.slogan{
+    position: relative;
+    left: -20%;
+    font-family: Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif;
+    
+}
+.logo{
+    margin-left: 65%;
+    margin-top: -17%;
+}
+.logo img{
+    width: 75%;
+    height: 75%;
+    object-fit: cover;
+}
+
+</style>
