592a03b008c437bf1ca3b3c4d2da62613f12d72e
SiveNcanywa <jeandreross@gmail.com>
2022-03-24 16:10:35 +0200

    changes

--- src/components/about.vue
+++ src/components/about.vue
@@ -20,16 +20,14 @@
     <tr>
       <td style="width: 30%; text-align: center;">
         <div>
-        <p style="text-align: center;"><img alt="Kavallerie about us page" src="https://i.postimg.cc/gJyVQW6m/Golden-Arrow-Bus-Service-logo-479x480.jpg" style="float: none;" /></p>
+        <p style="text-align: center;"><img alt="" src="https://i.postimg.cc/gJyVQW6m/Golden-Arrow-Bus-Service-logo-479x480.jpg" style="float: none;" /></p>
         </div>
       </td>
       
       <td style="width: 30%; text-align: center; height: 230px;">
 
         <p><span style="color: #fd7e14;"><b>ABOUT GOLDEN ARROW BUS SERVICES</b></span></p>
-<div><meta charset="utf-8" /></div>
-<div><meta charset="utf-8" /></div>
-<div><meta charset="utf-8" /></div>
+
 <p><strong>Golden Arrow Bus Services (GABS) is the major public transport bus service operator for Cape Town, South Africa, providing commuter bus services throughout a large part of the City of Cape Town metropolitan area.
 
 Founded in 1861, and owned by South African holding company Hosken Consolidated Investments Limited, the company operates a fleet of over 1000 buses on 1300 routes, with a daily ridership of around 220,000 people.</strong></p>
@@ -37,23 +35,23 @@ Founded in 1861, and owned by South African holding company Hosken Consolidated 
       </td>
             <td style="width: 30%; text-align: center; height: 230px;">
 <div></div>
-<div style="text-align: center;"><img alt="kavallerie about us page" src="https://i.postimg.cc/NF0T5rLP/bus1-1-479x480.png" style="float: none; width" /></div>
+<div style="text-align: center;"><img alt="" src="https://i.postimg.cc/NF0T5rLP/bus1-1-479x480.png" style="float: none; width" /></div>
 </td>
     </tr>
     <tr>
       <td style="width: 30%;">
-<div><meta charset="utf-8" /></div>
+<div></div>
 <p style="text-align: center;"><span style="color: #fd7e14;"><b>GABS'S MISSION</b></span></p>
-<div><meta charset="utf-8" /></div>
+<div></div>
 <p style="text-align: center;"><span><strong>Delivery of safe and reliable scheduled passenger transport. Staff satisfaction, upskilling and other employee benefits. Investment in the community through projects and monetary donations.</strong>  </span></p>
 </td>
       <td style="width: 30%; text-align: center;">
-<p style="text-align: center;"><img alt="Kavallerie about us page" src="https://i.postimg.cc/fRrkzrhX/bus2-479x480.jpg" style="float: none;" /></p>
+<p style="text-align: center;"><img alt="" src="https://i.postimg.cc/fRrkzrhX/bus2-479x480.jpg" style="float: none;" /></p>
 </td>
       <td style="width: 30%; text-align: center;">
-<div ><meta charset="utf-8" />
+<div >
 <p><span style="color: #fd7e14;"><b>WHY CHOOSE GABS?</b></span></p>
-<div><meta charset="utf-8" /><strong>We operate safe,reliable passenger transport services designed to meet the needs of our customers and by applying sound business principles to create a secure future for the company and its people.</strong></div>
+<div><strong>We operate safe,reliable passenger transport services designed to meet the needs of our customers and by applying sound business principles to create a secure future for the company and its people.</strong></div>
 </div>
 
 </td>
@@ -67,33 +65,12 @@ Founded in 1861, and owned by South African holding company Hosken Consolidated 
               <b>ABOUT GOLDEN ARROW BUS SERVICES</b>
           </span>
         </p>
-<div><meta charset="utf-8" /></div>
-<div><meta charset="utf-8" /></div>
-<div><meta charset="utf-8" /></div>
-<p><strong>Our founder, a material scientist and an avid equestrian enthusiast has been developing saddles and pads to meet the demands of high-performers like you. He traveled all over the world, found that most of the saddles are lacking design and functionality, that's where we started. </strong></p>
-</div>
-  <p style="text-align: center;"><span style="color: #fd7e14;"><b>GABS MISSION</b></span></p>
-<div><meta charset="utf-8" /></div>
-  <div id ="statement" >
-<p style="text-align: center;"><span><strong>We constantly strive to exceed the expectations of all riders. You and your horse’s safety is our main priority without compromising comfort and practicality. With KAVALLERIE equestrian gear, we'll ride to the victory together.</strong>  </span></p>
-  </div>
-  <div id ="statement" >
-<div><meta charset="utf-8" /><strong>We've innovated cutting-edge technologies for decades and had incorporated 3 key elements into every product: protection, comfort, and utility for both horse and rider. Whether you are in the game to win or riding for leisure, KAVALLERIE is your leading supplier for equestrian gear.</strong></div>
-   </div>
+
     <table id="tb_mobile">
     
-    <tr>
-     <td style="width: 30%; text-align: center;">
-        <p style="text-align: center;"><img alt="Kavallerie about us page" src="//cdn.shopify.com/s/files/1/1589/2323/files/about_us_1-min_large.png?v=1493264237" style="float: none; max-width: 200px; width: 90%" /></p>
-      </td>
-      <td style="width: 30%; text-align: center;">
-<img alt="Kavallerie about us page" src="//cdn.shopify.com/s/files/1/1589/2323/files/about_us_3-min_12bba61a-1cd0-4607-8476-9fba88c2bafd_large.png?v=1493264946" style="float: none; max-width: 200px; width: 90%" />
-</td>
-      <td style="width: 30%; text-align: center; height: 230px;">
-<div></div>
-  <img alt="kavallerie about us page" src="//cdn.shopify.com/s/files/1/1589/2323/files/about_us_2-min_large.png?v=1493264305" style="float: none; max-width: 200px; width: 90%" /></td>
-    </tr>
+  
     </table>
+</div>
 </div>
  
 </template>
@@ -108,16 +85,14 @@ export default {};
     }
     
 
-  .show_mobile{
-    display: none;
-  }
+  
   @media screen and (max-width: 650px) {
     .show_mobile{
       display: inline;
     }
     #tb td{
       border: 0;
-      display: none;
+      
     }
     #statement{
       margin:15px 10% 15px 10%;
