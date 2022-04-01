3703408f39661dc68b5e94a93723a0a77b9cb539
SiveNcanywa <jeandreross@gmail.com>
2022-04-01 12:18:10 +0200

    making the about page responsive

--- src/components/about.vue
+++ src/components/about.vue
@@ -16,98 +16,92 @@
     </div>
   </nav>
   <div id="about">
-  <table id="tb">
-    <tbody>
-      <tr>
-        <td  scope="col" style="width: 30%; text-align: center">
-          <div>
-            <p style="text-align: center">
-              <img
-                alt=""
-                src="https://i.postimg.cc/gJyVQW6m/Golden-Arrow-Bus-Service-logo-479x480.jpg"
-                style="float: none"
-              />
-            </p>
-          </div>
-        </td>
-
-        <td scope="col" style="width: 30%; text-align: center; height: 230px">
-          <p>
-            <span style="color: #fd7e14"
-              ><b>ABOUT GOLDEN ARROW BUS SERVICES</b></span
-            >
-          </p>
-
-          <p scope="col">
-            <strong
-              >Golden Arrow Bus Services (GABS) is the major public transport
-              bus service operator for Cape Town, South Africa, providing
-              commuter bus services throughout a large part of the City of Cape
-              Town metropolitan area. Founded in 1861, and owned by South
-              African holding company Hosken Consolidated Investments Limited,
-              the company operates a fleet of over 1000 buses on 1300 routes,
-              with a daily ridership of around 220,000 people.</strong
-            >
-          </p>
-        </td>
-        <td  scope="col" style="width: 30%; text-align: center; height: 230px">
-          <div></div>
-          <div style="text-align: center">
-            <img
-              alt=""
-              src="https://i.postimg.cc/NF0T5rLP/bus1-1-479x480.png"
-              style="float: none; width"
-            />
-          </div>
-        </td>
-      </tr>
-      <tr>
-        <td scope="col" style="width: 30%">
-          <div></div>
-          <p style="text-align: center">
-            <span style="color: #fd7e14"><b>GABS'S MISSION</b></span>
-          </p>
-          <div></div>
-          <p style="text-align: center">
-            <span
-              ><strong
-                >Delivery of safe and reliable scheduled passenger transport.
-                Staff satisfaction, upskilling and other employee benefits.
-                Investment in the community through projects and monetary
-                donations.</strong
-              > 
-            </span>
-          </p>
-        </td>
-        <td scope="col" style="width: 30%; text-align: center">
+    <div class="myRow">
+      <div class="data">
+        <div>
           <p style="text-align: center">
             <img
               alt=""
-              src="https://i.postimg.cc/fRrkzrhX/bus2-479x480.jpg"
+              src="https://i.postimg.cc/gJyVQW6m/Golden-Arrow-Bus-Service-logo-479x480.jpg"
               style="float: none"
             />
           </p>
-        </td>
-        <td scope="col" style="width: 30%; text-align: center">
+        </div>
+      </div>
+
+      <div class="data">
+        <p>
+          <span style="color: #fd7e14"
+            ><b>ABOUT GOLDEN ARROW BUS SERVICES</b></span
+          >
+        </p>
+
+        <p scope="col">
+          <strong
+            >Golden Arrow Bus Services (GABS) is the major public transport bus
+            service operator for Cape Town, South Africa, providing commuter bus
+            services throughout a large part of the City of Cape Town
+            metropolitan area. Founded in 1861, and owned by South African
+            holding company Hosken Consolidated Investments Limited, the company
+            operates a fleet of over 1000 buses on 1300 routes, with a daily
+            ridership of around 220,000 people.</strong
+          >
+        </p>
+      </div>
+      <div class="data">
+        <div></div>
+        <div style="text-align: center">
+          <img
+            alt=""
+            src="https://i.postimg.cc/NF0T5rLP/bus1-1-479x480.png"
+            style="float: none; width"
+          />
+        </div>
+      </div>
+
+      <div class="data">
+        <div></div>
+        <p style="text-align: center">
+          <span style="color: #fd7e14"><b>GABS'S MISSION</b></span>
+        </p>
+        <div></div>
+        <p style="text-align: center">
+          <span
+            ><strong
+              >Delivery of safe and reliable scheduled passenger transport.
+              Staff satisfaction, upskilling and other employee benefits.
+              Investment in the community through projects and monetary
+              donations.</strong
+            > 
+          </span>
+        </p>
+      </div>
+      <div class="data">
+        <p style="text-align: center">
+          <img
+            alt=""
+            src="https://i.postimg.cc/fRrkzrhX/bus2-479x480.jpg"
+            style="float: none"
+          />
+        </p>
+      </div>
+      <div class="data">
+        <div>
+          <p>
+            <span style="color: #fd7e14"><b>WHY CHOOSE GABS?</b></span>
+          </p>
           <div>
-            <p>
-              <span style="color: #fd7e14"><b>WHY CHOOSE GABS?</b></span>
-            </p>
-            <div>
-              <strong
-                >We operate safe,reliable passenger transport services designed
-                to meet the needs of our customers and by applying sound
-                business principles to create a secure future for the company
-                and its people.</strong
-              >
-            </div>
+            <strong
+              >We operate safe,reliable passenger transport services designed to
+              meet the needs of our customers and by applying sound business
+              principles to create a secure future for the company and its
+              people.</strong
+            >
           </div>
-        </td>
-      </tr>
-    </tbody>
- 
-  </table>
+        </div>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
@@ -119,5 +113,15 @@ export default {};
   height: 20px;
 }
 
+.myRow {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  width: 100%;
+}
 
+.data {
+  max-width: 500px;
+  width: 100%;
+}
 </style>
